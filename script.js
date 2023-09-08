0a4f1bdccd0de9ed930eef712a81a89c8144b663
AndyMagwayer <andymagwayer@gmial.com>
2023-09-08 19:09:40 +0500

    feat: completed fully project (19:09)

--- script.js
+++ script.js
@@ -1,12 +1,14 @@
-document.getElementById('generate-btn').addEventListener('click', function()){
+document.getElementById('generate-btn').addEventListener('click', function() {
     const color1 = document.getElementById('color1').value;
     const color2 = document.getElementById('color2').value;
     const borderRadius = document.getElementById('border-radius').value;
-
-
+  
     const gradientPreview = document.querySelector('.gradient-preview');
-    gradientPreview.style.background = 'linear-gradient(to right, ' + color1 + ',' + color2 + ')';
+    gradientPreview.style.background = 'linear-gradient(to right, ' + color1 + ', ' + color2 + ')';
     gradientPreview.style.borderRadius = borderRadius + 'px';
+  
+    var cssCode = 'background: linear-gradient(to right, ' + color1 + ', ' + color2 + ');\n';
+    cssCode += 'border-radius: ' + borderRadius + 'px;';
+    document.getElementById('css-code').value = cssCode;
+  });
 
-    
-}
